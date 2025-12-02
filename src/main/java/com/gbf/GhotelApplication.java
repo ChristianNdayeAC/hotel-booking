aab0cd7c8cbbc0533b05ed2dff8a56307f5b53bd
Christian Ndaye <christianndaye.ac@gmail.com>
2025-12-02 14:24:32 -0800

    upload the project folder to repo

---
+++ src/main/java/com/gbf/GhotelApplication.java
@@ -0,0 +1,13 @@
+package com.gbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhotelApplication.class, args);
+	}
+
+}
